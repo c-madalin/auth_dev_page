570326dff967a43de7fd2e5b992d6a29965f3f60
[cazan] <cazanmadalin12@gmail.com>
2025-07-28 00:04:45 +0300

    merge step 1

--- seller2/src/app/components/step1/step1.component.ts
+++ seller2/src/app/components/step1/step1.component.ts
@@ -1,15 +1,16 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-step1',
-  imports: [],
   standalone: true,
+  imports: [],
   templateUrl: './step1.component.html',
   styleUrls: ['./step1.component.css']
 })
 export class Step1Component {
   @Output() next = new EventEmitter<void>();
-   months = [
+
+  months = [
     'January', 'February', 'March', 'April', 'May', 'June',
     'July', 'August', 'September', 'October', 'November', 'December'
   ];
