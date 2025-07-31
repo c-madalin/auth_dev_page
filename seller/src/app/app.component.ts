af8836ce416ed1f97caaaa543626434d9d346bc3
[cazan] <cazanmadalin12@gmail.com>
2025-07-31 21:21:23 +0300

    yeap

--- seller/src/app/app.component.ts
+++ seller/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ContainerComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'seller';
