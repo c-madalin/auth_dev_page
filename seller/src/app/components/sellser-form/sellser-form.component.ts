55fd7e8738120399bbb96d6bf615e7e7cd4fd710
[cazan] <cazanmadalin12@gmail.com>
2025-07-31 21:06:52 +0300

    Components, created

---
+++ seller/src/app/components/sellser-form/sellser-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sellser-form',
+  imports: [],
+  templateUrl: './sellser-form.component.html',
+  styleUrl: './sellser-form.component.css'
+})
+export class SellserFormComponent {
+
+}
