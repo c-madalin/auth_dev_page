410038601bd5364198d6c9373397c21c38741bdf
[cazan] <cazanmadalin12@gmail.com>
2025-08-01 15:10:43 +0300

    navigation bun

---
+++ seller/src/app/components/navigation-buttons/navigation-buttons.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-buttons',
+  standalone: true,
+  imports: [],
+  templateUrl: './navigation-buttons.component.html',
+  styleUrl: './navigation-buttons.component.css'
+})
+export class NavigationButtonsComponent {
+  @Input() step!: number;
+  @Input() maxStep = 6;
+
+  @Output() next = new EventEmitter<void>();
+  @Output() prev = new EventEmitter<void>();
+
+  goNext() {
+    this.next.emit();
+  }
+
+  goPrev() {
+    this.prev.emit();
+  }
+}
