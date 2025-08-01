b9ac97bee131bd941b240993309ee5d388a46114
[cazan] <cazanmadalin12@gmail.com>
2025-08-01 12:15:22 +0300

    another sae before step 1

--- seller/src/app/components/seller-form/seller-form.component.ts
+++ seller/src/app/components/seller-form/seller-form.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { Step5Component } from "../step5/step5.component";
+import { Step6Component } from "../step6/step6.component";
+import { Step4Component } from "../step4/step4.component";
+import { Step3Component } from "../step3/step3.component";
+import { Step2Component } from "../step2/step2.component";
+import { Step1Component } from "../step1/step1.component";
+import { NgSwitch, NgSwitchCase } from '@angular/common';
+
 
 @Component({
   selector: 'app-seller-form',
-  imports: [],
+  imports: [Step5Component, Step6Component, Step4Component, Step3Component, Step2Component, Step1Component, NgSwitch, NgSwitchCase],
   templateUrl: './seller-form.component.html',
   styleUrl: './seller-form.component.css'
 })
-export class SellerFormComponent {
 
+export class SellerFormComponent {
+  step = 1;
+
+  nextStep() {
+    if (this.step < 6) this.step++;
+  }
+
+  prevStep() {
+    if (this.step > 1) this.step--;
+  }
+
+  submitForm() {
+    // TODO: Trimite datele
+  }
 }
