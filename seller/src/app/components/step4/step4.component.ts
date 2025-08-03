284cd452b935d1b04e11afba4c637cdddf37fb90
[cazan] <cazanmadalin12@gmail.com>
2025-08-03 21:29:21 +0300

    sadas

--- seller/src/app/components/step4/step4.component.ts
+++ seller/src/app/components/step4/step4.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { NgFor} from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-step4',
   imports: [ NgFor, FormsModule, CommonModule],
+  standalone:true,
   templateUrl: './step4.component.html',
   styleUrls: ['./step4.component.css']
 })
