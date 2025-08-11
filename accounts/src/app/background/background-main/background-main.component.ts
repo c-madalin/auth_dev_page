fdae5eeeb437b182f22995eaa6becbb5e14b94bb
[cazan] <cazanmadalin12@gmail.com>
2025-08-11 11:29:41 +0300

    2 components
    background aand container creted

---
+++ accounts/src/app/background/background-main/background-main.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-main',
+  imports: [],
+  templateUrl: './background-main.component.html',
+  styleUrl: './background-main.component.css'
+})
+export class BackgroundMainComponent {
+
+}
