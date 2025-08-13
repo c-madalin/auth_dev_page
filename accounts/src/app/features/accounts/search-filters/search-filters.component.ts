4b72cad4e0b2254ac7cfc796ce0447bbd3ccbb9d
[cazan] <cazanmadalin12@gmail.com>
2025-08-13 12:44:48 +0300

    save

---
+++ accounts/src/app/features/accounts/search-filters/search-filters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-filters',
+  imports: [],
+  templateUrl: './search-filters.component.html',
+  styleUrl: './search-filters.component.css'
+})
+export class SearchFiltersComponent {
+
+}
