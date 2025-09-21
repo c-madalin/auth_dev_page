cf4c0441e32b9e49851e85153d51fd76b408cd93
[cazan] <cazanmadalin12@gmail.com>
2025-09-22 00:37:14 +0300

    save1

--- accounts2/src/app/madalin/background/overlay/overlay.component.ts
+++ accounts2/src/app/madalin/background/overlay/overlay.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+// overlay.component.ts
+import { Component, EventEmitter, HostListener, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-overlay',
-  imports: [],
-  templateUrl: './overlay.component.html',
-  styleUrl: './overlay.component.css'
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <div class="overlay" (click)="close.emit()" role="dialog" aria-modal="true">
+      <ng-content></ng-content>
+    </div>
+  `,
+  styleUrls: ['./overlay.component.css']
 })
 export class OverlayComponent {
+  @Output() close = new EventEmitter<void>();
 
+  @HostListener('document:keydown', ['$event'])
+  onKey(ev: KeyboardEvent) {
+    if (ev.key === 'Escape') this.close.emit();
+  }
 }
